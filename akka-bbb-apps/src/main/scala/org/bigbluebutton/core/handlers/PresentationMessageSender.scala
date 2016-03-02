177a658e2207a76f734ee2550fd41b6d7395753d
Anton Georgiev <anto.georgiev@gmail.com>
2016-03-02 15:59:09 +0000

    clean up

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/handlers/PresentationMessageSender.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/handlers/PresentationMessageSender.scala
@@ -11,19 +11,16 @@ trait PresentationMessageSender {
 
   def sendClearPresentation(meetingId: IntMeetingId, recorded: Recorded): Unit = {
     outGW.send(new ClearPresentationOutMsg(meetingId, recorded))
-    println("              sendClearPresentation")
   }
 
   def sendRemovePresentation(meetingId: IntMeetingId, recorded: Recorded, presentationId: PresentationId): Unit = {
     outGW.send(new RemovePresentationOutMsg(meetingId, recorded, presentationId))
-    println("                  sendRemovePresentation")
   }
 
   def sendGetPresentationInfo(): Unit = {
   }
   def sendGetPresentationInfo(meetingId: IntMeetingId, recorded: Recorded, userId: IntUserId,
     presentationInfo: CurrentPresentationInfo, replyTo: ReplyTo): Unit = {
-    println("               sendGetPresentationInfo")
     outGW.send(new GetPresentationInfoOutMsg(meetingId, recorded, userId, presentationInfo, replyTo))
   }
 }

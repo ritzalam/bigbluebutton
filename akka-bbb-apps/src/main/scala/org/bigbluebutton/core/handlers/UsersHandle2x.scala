6294aad20e52a21333aebfb520d62b9fe0b40b80
Richard Alam <ritzalam@gmail.com>
2016-03-15 16:10:14 +0000

    Update user handler
     Handle register user and join user messages.

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/handlers/UsersHandle2x.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/handlers/UsersHandle2x.scala
@@ -2,14 +2,15 @@ package org.bigbluebutton.core.handlers
 
 import org.bigbluebutton.core.api.{ UserJoining, ValidateAuthToken, RegisterUser }
 import org.bigbluebutton.core.domain.Muted
-import org.bigbluebutton.core.models.RegisteredUsers2x
+import org.bigbluebutton.core.models.{ Users2x, RegisteredUsers2x }
 import org.bigbluebutton.core.{ OutMessageGateway, LiveMeeting }
 
 trait UsersHandler2x extends UsersApp with UsersMessageSender {
   this: LiveMeeting =>
 
   val outGW: OutMessageGateway
-  val registeredUsers: RegisteredUsers2x
+  val registeredUsers2x: RegisteredUsers2x
+  val users2x: Users2x
 
   def handleRegisterUser2x(msg: RegisterUser): Unit = {
     val regUser = RegisteredUsers2x.create(msg.userId, msg.extUserId, msg.name, msg.roles, msg.authToken)
@@ -18,7 +19,7 @@ trait UsersHandler2x extends UsersApp with UsersMessageSender {
   }
 
   def handleValidateAuthToken2x(msg: ValidateAuthToken): Unit = {
-    meeting.findWithToken(msg.token) match {
+    registeredUsers2x.findWithToken(msg.token) match {
       case Some(u) =>
         sendValidateAuthTokenReplyMessage(props.id, msg.userId, msg.token, true, msg.correlationId)
       case None =>
@@ -27,25 +28,18 @@ trait UsersHandler2x extends UsersApp with UsersMessageSender {
   }
 
   def handleUserJoin2x(msg: UserJoining): Unit = {
-    val regUser = registeredUsers.findWithToken(msg.token)
-    val webUser = meeting.getUser(msg.userId)
-    webUser foreach { wu =>
-      if (!wu.joinedWeb.value) {
-        /**
-         * If user is not joined through the web (perhaps reconnecting).
-         * Send a user left event to clear up user list of all clients.
-         */
-        sendUserLeftEvent(wu)
-      }
-    }
+    // Check if there is a registered user with token
+    // Check if there is a user already in the list of users, if so, might be a reconnect
+    // Compare sessionId, if sessionId is not same then this is a reconnect
+    // Just update the sessionId and send join success
+    val regUser = registeredUsers2x.findWithToken(msg.token)
+    val users = users2x.toVector
+    val u = Users2x.findWithId(msg.userId, users)
 
     regUser foreach { ru =>
       val voiceUser = initializeVoice(msg.userId, ru.name)
       val locked = getInitialLockStatus(ru.roles)
       val uvo = createNewUser(msg.userId, ru.extId, ru.name, ru.roles, voiceUser, getInitialLockStatus(ru.roles))
-
-      log.info("User joined meeting. metingId=" + props.id + " userId=" + uvo.id + " user=" + uvo)
-
       sendUserJoinedMessage(props.id, props.recorded, uvo)
       sendMeetingStateMessage(props.id, props.recorded, uvo.id, meeting.getPermissions,
         Muted(meeting.isMeetingMuted))

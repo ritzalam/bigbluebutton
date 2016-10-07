f9e3f11d08adb2012c73ea79a098b614a7e31628
Richard Alam <ritzalam@gmail.com>
2016-10-07 20:51:52 +0000

    - move client actor

---
+++ incubator/bbb-apps/src/main/scala/org/bigbluebutton/core/api/events/Events.scala
@@ -0,0 +1,12 @@
+package org.bigbluebutton.core.api.events
+
+import org.bigbluebutton.core.domain.{ClientId, IntUserId}
+
+trait EventBody
+case class EventHeader(val name: String, val seqNum: Long, val prettyTs: String, val ts: Long)
+case class BbbEvent(header: EventHeader, body: EventBody)
+case class ClientJoined(userId: IntUserId, clientId: ClientId) extends EventBody
+case class VoiceMuted(userId: IntUserId, clientId: ClientId, muted: Boolean) extends EventBody
+
+
+
--- incubator/bbb-apps/src/main/scala/org/bigbluebutton/core/client/ClientActor.scala
+++ incubator/bbb-apps/src/main/scala/org/bigbluebutton/core/client/ClientActor.scala
@@ -1,6 +1,7 @@
-package org.bigbluebutton.core
+package org.bigbluebutton.core.client
 
-import akka.actor.{ Actor, ActorContext, ActorLogging, Props }
+import akka.actor.{ Actor, ActorLogging, Props }
+import org.bigbluebutton.core.{ IncomingEventBus2x, OutgoingEventBus }
 
 object ClientActor {
   def props(incomingEventBus2x: IncomingEventBus2x, outgoingEventBus: OutgoingEventBus): Props =
